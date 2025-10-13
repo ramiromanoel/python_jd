fc3e46520f4666fc32e51f0ea73d06a56b4a1306
ramiromanoel <ramiro.junior1@gmail.com>
2025-10-13 20:18:19 -0300

    atualizacao diretorio 04

---
+++ cl_04/lt_dc.py
@@ -0,0 +1,13 @@
+produto1: str = "sapato"
+produto2: str = "camiseta"
+produto3: str = "videogame"
+
+produtos: list = []
+
+produtos.append(produto1)
+produtos.append(produto2)
+produtos.append(produto3)
+produtos.pop()
+produtos.pop()
+
+print(produtos)
